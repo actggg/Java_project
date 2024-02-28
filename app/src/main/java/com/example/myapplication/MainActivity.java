bf2e39f56880d2d44f48849bc7f6a1ede58978c3
yuraz <yur.raz@mail.ru>
2024-02-29 00:53:25 +0300

    4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,56 +2,108 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.graphics.Color;
 import android.app.AlertDialog;
-import android.content.Context;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    public String my_word = "лампа";
     public int number_str = 1;
+    public String my_word;
 
-    // Переменная для хранения названий 25 полей ввода
+    SharedPreferences sPref;
+    public String my_button;
+    public int num = 1;
+    int lvl_in_main = 1;
+    int lvl_in_easy = 1;
+    int lvl_in_medium = 1;
+    int lvl_in_hard = 1;
+
     public EditText[] k;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView tv = findViewById(R.id.textViewTitle);
-        tv.setTextAppearance(this, R.style.CustomButtonFont);
-        //Button b = findViewById(R.id.buttonSubmit);
-        //b.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-          //  public void onClick(View v) {
-           //     word();
-           // }
-       // });
+        loadText();
+        menu();
     }
 
-    public void word(){
-        setContentView(R.layout.word);
+    public void menu() {
+        setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.buttonWordOfTheDay);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                word("buttonWordOfTheDay");
+            }
+        });
+        Button buttonlevels = findViewById(R.id.buttonlevels);
+        buttonlevels.setText("Прохождение уровней \n Уровень " + lvl_in_main + "/100");
+        buttonlevels.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                word("buttonlevels");
+            }
+        });
+        Button buttoneasy = findViewById(R.id.button_easy);
+        buttoneasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                word("button_easy");
+            }
+        });
+        Button buttonmed = findViewById(R.id.button_medium);
+        buttonmed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                word("button_medium");
+            }
+        });
+        Button buttonhard = findViewById(R.id.button_hard);
+        buttonhard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                word("button_hard");
+            }
+        });
+        Button buttonfriend = findViewById(R.id.buttonWordOfTheDay);
+        buttonhard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveText();
+            }
+        });
+    }
+
+    public void word(String clicked_button) {
+        setContentView(R.layout.word);
+        TextView tx = findViewById(R.id.wordli_text);
+        tx.setText("Уровень № " + check_lvl(clicked_button));
+        my_button = clicked_button;
+        my_word = work_with_file(clicked_button);
+        number_str = 1;
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
                 EditText editText = findViewById(getResources().getIdentifier("letter_input" + i + "_" + j, "id", getPackageName()));
@@ -64,26 +116,45 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.letter_input3_1), findViewById(R.id.letter_input3_2), findViewById(R.id.letter_input3_3), findViewById(R.id.letter_input3_4), findViewById(R.id.letter_input3_5),
                 findViewById(R.id.letter_input4_1), findViewById(R.id.letter_input4_2), findViewById(R.id.letter_input4_3), findViewById(R.id.letter_input4_4), findViewById(R.id.letter_input4_5),
                 findViewById(R.id.letter_input5_1), findViewById(R.id.letter_input5_2), findViewById(R.id.letter_input5_3), findViewById(R.id.letter_input5_4), findViewById(R.id.letter_input5_5),};
-        // Заполнение массива с названиями полей ввода
-        Button button = findViewById(R.id.check_button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        ImageButton back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                check(number_str);
+                menu();
+            }
+        });
+        Button check_button = findViewById(R.id.check_button);
+        check_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                check(number_str, clicked_button);
             }
         });
     }
-    public void trueWord(int num, String text, boolean triger) {
+
+    public void dialog(String text){
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(text)
+            .setPositiveButton("OK", null)
+                    .show();
+    menu();}
+
+    public void trueWord(int num, String clicked_button) {
         String str = "";
         for (int j = 1; j <= 5; j++) {
             EditText editText = findViewById(getResources().getIdentifier("letter_input" + num + "_" + j, "id", getPackageName()));
             str += editText.getText().toString();
         }
-        if (str.equals(my_word) || !triger) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage(text)
-                    .setPositiveButton("OK", null)
-                    .show();
+        if (str.equals(my_word)) {
+            dialog("Всё верно!!!");
+            next_lvl(clicked_button);
+            menu();
+        }
+        else if (num == 5){
+            number_str = 1;
+            dialog("Вы ошиблись!!!");
+            menu();
         }
     }
 
@@ -100,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void check(int num) {
+    public void check(int num, String clicked_button) {
         int empty = 0;
         for (int i = 0; i < 5; i++) {
             if (k[i + (num - 1) * 5].getText().toString().isEmpty()) {
@@ -124,16 +195,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
                 number_str++;
-                if (number_str <= 5) {
-                    block(number_str);
-                    trueWord(num, "Все верно!!!", true);
-                } else if (number_str == 6) {
-                    trueWord(num, "Все верно!!!", true);
-                } else {
-                    trueWord(num, "К сожалению вы ошиблись", false);
-                    number_str = 1;
-                    block(number_str);
-                }
+                block(number_str);
+                trueWord(num, clicked_button);
             }
         } else {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -149,6 +212,109 @@ public class MainActivity extends AppCompatActivity {
         return word.contains(letter);
     }
 
+    public void next_lvl(String clicked_button) {
+        if (clicked_button == "button_easy"){
+            lvl_in_easy++;
+        }
+        if (clicked_button == "button_medium"){
+            lvl_in_medium++;
+        }
+        if (clicked_button == "button_hard"){
+            lvl_in_hard++;
+        }
+        if (clicked_button == "buttonlevels"){
+            lvl_in_main++;
+        }
+    }
+
+    public String file_buy_but(String clicked_button) {
+        String file = "nn_words.txt";
+        if (clicked_button == "button_easy"){
+            file = "words_easy.txt";
+        }
+        if (clicked_button == "button_medium"){
+            file = "words_medium.txt";
+        }
+        if (clicked_button == "button_hard"){
+            file = "words_hard.txt";
+        }
+        if (clicked_button == "buttonlevels"){
+            file = "go_lvls.txt";
+        }
+        return file;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveText();
+    }
+
+    void saveText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sPref.edit();
+        editor.putString("lvl_in_main", String.valueOf(lvl_in_main));
+        editor.putString("lvl_in_easy", String.valueOf(lvl_in_easy));
+        editor.putString("lvl_in_medium", String.valueOf(lvl_in_medium));
+        editor.putString("lvl_in_hard", String.valueOf(lvl_in_hard));
+        editor.apply();
+    }
+
+    void loadText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        lvl_in_main = Integer.parseInt(sPref.getString("lvl_in_main", "2"));
+        lvl_in_easy = Integer.parseInt(sPref.getString("lvl_in_easy", "2"));
+        lvl_in_medium = Integer.parseInt(sPref.getString("lvl_in_medium", "2"));
+        lvl_in_hard = Integer.parseInt(sPref.getString("lvl_in_hard", "2"));
+    }
+
+
+
+    public int check_lvl(String clicked_button){
+        if (clicked_button == "button_easy"){
+            return lvl_in_easy;
+        }
+        if (clicked_button == "button_medium"){
+            return lvl_in_medium;
+        }
+        if (clicked_button == "button_hard"){
+            return lvl_in_hard;
+        }
+        if (clicked_button == "buttonlevels"){
+            return lvl_in_main;
+        }
+        return 0;
+    }
+
+
+
+
+
+    public String work_with_file(String clicked_button) {
+        String filePath = file_buy_but(clicked_button);
+        int lvl = check_lvl(clicked_button);
+        // Путь к папке Assets
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open(filePath)));
+
+            String line;
+            int lineCount = 1;
+            while (lineCount <= 100) {
+                line = br.readLine();
+                // Если номер строки совпадает с заданным, возвращаем ее
+                if (lineCount == lvl) {
+                    return line;
+                }
+                lineCount++;
+            }
+            return "";
+        }
+        catch (IOException e){
+            return "лампа";
+        }
+    }
+
     public boolean isLetterAtIndex(String word, String letter, int index) {
         if (index >= 0 && index < word.length()) {
             char charAtIndex = word.charAt(index);
@@ -161,6 +327,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void applyRussianLettersFilter(EditText editText) {
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_DEL && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    EditText previousEditText = (EditText) editText.focusSearch(View.FOCUS_LEFT);
+                    if (previousEditText != null) {
+                        previousEditText.requestFocus();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -175,12 +355,13 @@ public class MainActivity extends AppCompatActivity {
                 String input = s.toString();
                 if (!input.matches("[а-яА-Я]*")) {
                     s.replace(0, s.length(), getRussianLettersFromString(input));
-                } else {
+                } else if (input.length() > 0) {
                     moveToNextField(editText);
                 }
             }
         });
     }
+
 
     private void moveToNextField(EditText currentEditText) {
         View next = currentEditText.focusSearch(View.FOCUS_RIGHT);
@@ -202,7 +383,7 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean check_in_file(String s) {
         try {
-            String filePath = "nn_words.txt"; // Имя файла в папке assets
+            String filePath = "nn_words.txt";
 
             if (isWordPresent(s, filePath)) {
                 return true;
@@ -219,7 +400,8 @@ public class MainActivity extends AppCompatActivity {
         HashMap<String, Integer> wordMap = new HashMap<>();
 
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(filePath)));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(filePath), "UTF-8"));
+
             String line;
 
             while ((line = reader.readLine()) != null) {
